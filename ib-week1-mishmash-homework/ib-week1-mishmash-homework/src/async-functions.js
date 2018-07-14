ba22561d202e661bd69a65e9a9f2f7954fad975c
993178 <papilio29@hotmail.com>
2018-07-14 19:50:03 +0200

    ik geef het op. F it. Ik ga wel shoppen

--- ib-week1-mishmash-homework/ib-week1-mishmash-homework/src/async-functions.js
+++ ib-week1-mishmash-homework/ib-week1-mishmash-homework/src/async-functions.js
@@ -5,99 +5,33 @@ function giveItBackLater(value, callback) {
      setTimeout(waitASec, 2000)
 }
 
-giveItBackLater('Tadaa!', console.log);
-
 function promiseToGiveItBackLater(value) {
     const promise = new Promise((resolve,reject) => {
         giveItBackLater(value, console.log);
+        return value
+    })
+    return promise
+}
+
+promiseToGiveItBackLater('Tadaa!')
+    .then(value => console.log(value))
+    .catch(value => console.log(value, 'nope'))
+
+
+function addSomePromises(parSomePromise) {
+    return new Promise((resolve,reject) => {
+        parSomePromise = Math.random()
+        if (parSomePromise < 0.5) {
+            return resolve('foo')
+        } else {
+            return reject('bar')
+        }
     })
 }
 
-promiseToGiveItBackLater('tja')
-    .then(waarde => console.log(waarde))
-    .catch(waarde => console.log(waarde, 'nope'))
+addSomePromises('iets')
+    .then(value => console.log(value + value))
+    .catch(value => console.error(value + value + value))
 
 
-//X  The function `promiseToGiveItBackLater(value)` should return a promise 
-//  that will resolve with the `value` later. 
-// It should use the same function you defined in `giveItBackLater`. 
-//That means you will wrap your callback-style function with a promise-style version.
-
-
-// const sayMyName = (name) => {
-//     const promise = new Promise((resolve, reject) => {
-//       setTimeout(reject, 1000, name)
-//     })
-  
-//     return promise
-//   }
-  
-//   sayMyName('Henk')
-//     .then(value => console.log("Hey!", value))
-//     .catch(value => console.log("REJECTED!", value),)
-
-
-
-
-
-// function addSomePromises(parSomePromise) {
-//     return new Promise((resolve,reject) => {
-//         parSomePromise = Math.random()
-//         if (parSomePromise < 0.5) {
-//             return resolve('foo')
-//         } else {
-//             return reject('bar')
-//         }
-//     })
-// }
-
-
-
-//volgende functie
-    
-//}
-
-//module.exports = {giveItBackLater, addSomePromises, promiseToGiveItBackLater}
-
-
-//info verzamelen
-// const promise = new Promise((resolve, reject) => {
-//     randomNumber = Math.random()
-//     if(randomNumber < 0.5) {
-//         return reject("bar")
-//     }
-//     return resolve("foo")
-//   })
-  
-//   promise
-//     .then(value => console.log(value + value))
-//     .catch(value => console.error(value + value + value))
-
-
-
-
-    // function getDocument(documentId) {
-    //     return new Promise((resolve, reject) => {
-    //       if(documents[documentId] === undefined){
-    //         setTimeout(reject, 1000, 'Document not found, sorry')
-    //       }
-    //       setTimeout(resolve, 1000, documents[documentId])
-    //     })
-    //   }
-      
-//       function getAuthor(authorId) {
-//         return new Promise((resolve, reject) => {
-//           if(authors[authorId] === undefined){
-//             setTimeout(reject, 1000, 'Author not found')
-//           }
-//           setTimeout(resolve, 1000, authors[authorId])
-//         }) 
-//       }
-      
-//       const nameToUppercase = name => name.toUpperCase()
-      
-//       getDocument(1)
-//         .then(document => getAuthor(document.authorId))
-//         .then(author => nameToUppercase(author.name))
-//         .then(upperCaseAuthor => console.log(upperCaseAuthor))
-//         .catch(message => console.log(message))
+module.exports = {giveItBackLater, addSomePromises, promiseToGiveItBackLater}
--- ib-week1-mishmash-homework/ib-week1-mishmash-homework/src/pathFind.js
+++ ib-week1-mishmash-homework/ib-week1-mishmash-homework/src/pathFind.js
@@ -1,35 +1,18 @@
 function pathFind(path, object) {
    const path2 = path[0]
-   const object2 = (object[path2])
-    console.log(object2)
+   console.log(path2)
+   const object2 = object.path2
+       console.log(object.author)
         path2.shift
         console.log(path2)
    if (path2.length >= 1) {
         return pathFind(path2,object2)
         console.log(pathFind(path2,object2))
    } else {
-        console.log('einde')
+        return object2
    }
 }
 
-// function pathFind(path, object) {
-//     const path2 = path[0]
-//          console.log(path2)
-//     object2 = object.path2
-//          console.log(object2)
-//     path2.shift
-//          console.log(path2)
-//     return pathFind(path2, object2);
-// }
-
-function pathFind(path, object) {
-    
-}
-
-
-
-pathFind(["author", "name"], { author: { name: "Stephen" } })
-
-//uitkomst: value van de key die in de array genoemd wordt
+console.log(pathFind(["author", "name"], { author: { name: "Stephen" } }))
 
 module.exports = {pathFind}
