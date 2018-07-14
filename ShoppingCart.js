ac556615ff9728868881cbc1222eaa9b6789ea86
993178 <papilio29@hotmail.com>
2018-07-14 15:12:53 +0200

    first thingies

---
+++ ShoppingCart.js
@@ -0,0 +1,28 @@
+class ShoppingCart {
+    constructor() {
+        this.items = items
+    }
+    getItems(item) {
+        this.items.push(item)
+    }
+}
+
+
+
+function clone() {
+    new ShoppingCart {
+        ...cart
+    }
+}
+
+function clear() {
+    
+}
+
+
+
+
+
+module.exports = {ShoppingCart}function getItems(item) {
+
+}
